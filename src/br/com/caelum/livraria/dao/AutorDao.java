8f8f9b0f18a353738448294c677856ac661e59a3
Johnny Silva <johnnysilva_estagio@hotmail.com>
2018-10-25 09:58:47 -0300

    Ciclo de vida dos Session Beans

--- src/br/com/caelum/livraria/dao/AutorDao.java
+++ src/br/com/caelum/livraria/dao/AutorDao.java
@@ -2,17 +2,30 @@ package br.com.caelum.livraria.dao;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 
 import br.com.caelum.livraria.modelo.Autor;
 
 @Stateless
 public class AutorDao {
 
-	private Banco banco = new Banco();
+	@Inject
+	private Banco banco ;
 
+	
 	public void salva(Autor autor) {
+		System.out.println("Salvando Autor" + autor.getNome());
+		
+		try {
+			Thread.sleep(20000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		banco.save(autor);
+		System.out.println("Salvou o Autor: " + autor.getNome());
 	}
 	
 	public List<Autor> todosAutores() {
@@ -22,6 +35,11 @@ public class AutorDao {
 	public Autor buscaPelaId(Integer autorId) {
 		Autor autor = this.banco.buscaPelaId(autorId);
 		return autor;
+	}
+	
+	@PostConstruct
+	void aposCriacao() {
+	    System.out.println("AutorDao foi criado");
 	}
 	
 }
--- src/br/com/caelum/livraria/dao/Banco.java
+++ src/br/com/caelum/livraria/dao/Banco.java
@@ -3,10 +3,17 @@ package br.com.caelum.livraria.dao;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
 import br.com.caelum.livraria.modelo.Autor;
 import br.com.caelum.livraria.modelo.Livro;
 import br.com.caelum.livraria.modelo.Usuario;
 
+
+@Singleton
+@Startup
 public class Banco {
 	
 	public static List<Livro> livros = new ArrayList<Livro>();
@@ -76,6 +83,11 @@ public class Banco {
 		livros.add(new Livro("JSF Efficaz",coelho));
 		
 		usuarios.add(new Usuario("admin", "pass"));
+	}
+	
+	@PostConstruct
+	void aposCriacao() {
+	    System.out.println("acabou de criar o Banco");
 	}
 
 }
